311b1c37a1cf3a46abd4e5fe40f8c4c2e7e83f9a
Suman-Rajput <sumanrajputmit@gmail.com>
2025-02-01 19:52:58 +0530

    first POC

---
+++ poc_submodule/src/poc_submodule/Hello_projectA.java
@@ -0,0 +1,11 @@
+package poc_submodule;
+
+public class Hello_projectA {
+
+	public static void main(String[] args) {
+		System.out.println("hello new push ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
